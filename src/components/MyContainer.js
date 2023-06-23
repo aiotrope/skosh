45bb3191dcf85896abea16aa1dd31c2305785fdd
aiotrope <arneli74@protonmail.com>
2023-06-23 21:09:19 +0300

    ✨ feat(app): solutions for exercise 11

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,19 +1,66 @@
 import { useState } from 'react'
 import MyList from './MyList'
 
+const AddForm = (props) => {
+  const [text, setText] = useState('')
+
+  const onChange = (event) => {
+    event.persist()
+
+    setText(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+    if (event) event.preventDefault()
+
+    let last_item = props.items[props.items.length - 1] // last element
+
+    let inncrement_id = parseInt(last_item.id) + 1
+
+    let data = { id: inncrement_id.toString(), text: text, clicked: false }
+
+    props.setItems((prevItemsState) => [...prevItemsState, data])
+
+    setText('')
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <p>
+        <label htmlFor='add-item'>Item Form</label>
+      </p>
+      <textarea id='add-item' value={text} onChange={onChange} placeholder='Add an item' />
+      <br />
+      <button type='submit'>ADD</button>
+    </form>
+  )
+}
+
 const MyContainer = () => {
   const title = 'Really epic list component'
 
   const list = [
-    { id: '1', text: 'This is an item' },
-    { id: '2', text: 'Also this' },
+    { id: '1', text: 'This is an item', clicked: false },
+    { id: '2', text: 'Also this', clicked: false },
   ]
   const [items, setItems] = useState(list)
-  
 
+  const updateItem = (id) => {
+    let _id = parseInt(id)
+
+    let newItems = [...items]
+
+    newItems[_id] = { ...items[_id], clicked: true }
+
+    console.log(newItems)
+
+    setItems(newItems)
+  }
+  console.log(items)
   return (
     <div>
-      <MyList header={title} items={list} />
+      <AddForm items={items} setItems={setItems} />
+      <MyList header={title} items={items} updateItem={updateItem} />
     </div>
   )
 }
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,11 +1,18 @@
 const MyList = (props) => {
   const list = props.items
+
   return (
     <>
       <h1>{props.header}</h1>
       <ol>
-        {list.map((el) => (
-          <li key={el.id}>{el.text}</li>
+        {list.map((element, idx) => (
+          <li
+            key={idx}
+            onClick={() => props.updateItem(idx)}
+            className={element.clicked ? 'li-linethrough' : null}
+          >
+            {element.text}
+          </li>
         ))}
       </ol>
     </>
